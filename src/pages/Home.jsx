8ca68fb31f7efb769cd597387abbebc86bb43ecd
ALPINISTLAR <niveam424@gmail.com>
2024-05-30 17:47:01 +0200

    Add HomeHero

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HomeHero from '../components/home/HomeHero'
+
+const Home = () => {
+  return (
+    <div className='pages'>
+      <HomeHero />
+    </div>
+  )
+}
+
+export default Home
